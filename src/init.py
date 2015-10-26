6c40e30700db368a556106ed88cb99ae2ca6a8fb
fergals <ferghal@gmail.com>
2015-10-26 16:04:46 +0000

    Moved tpo sqlite

--- src/init.py
+++ src/init.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template
-from movies import Content
+from flask import Flask, render_template, g
+import sqlite3
 
-TOPIC_DICT = Content()
+DATABASE = 'movieinfo.db'
 
 app = Flask(__name__)
+app.config.from_object(__name__) ##connect to config file
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE']) ##connect to database
 
 @app.route('/')
 def homepage():
-    return render_template("main.html")
+	g.db = connect_db()
+	cur = g.db.execute('select movie_name, director, year, gross, poster, category, studio from movies') 
+	movies = [dict(movie_name=row[0], director=row[1], year=row[2], gross=row[3], poster=row[4], category=row[5], studio=row[6])  for row in cur.fetchall()] #adds database data to dictionary
+	g.db.close()
+	return render_template('main.html', movies=movies)
 	
 @app.route ('/movies/')
 @app.route ('/movies/<name>/')
-def hello ( name = None ):
-	return render_template ('categories.html', name=name, TOPIC_DICT = TOPIC_DICT)
-
+def movies ( name = None ):
+	return render_template ('categories.html', name=name)
+	
+@app.route ('/movies/genre/<name>/')
+def genre ( name = None ):
+	return render_template ('genre.html', name=name)
+	
 @app.errorhandler(404)
 def page_not_found(error):
 	return render_template("404.html")
