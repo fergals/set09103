9032c4ed379788a48728f91f0dc53da0186d9c4d
fergals <ferghal@gmail.com>
2015-10-27 18:25:57 +0000

    Added individual movie page and rating images

--- src/init.py
+++ src/init.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def homepage(name=None):
     g.db = connect_db()
-    cur = g.db.execute('select id, movie_name, poster from movies')
-    movies = [dict(id=row[0], movie_name=row[1], poster=row[2]) for row in cur.fetchall()]
+    cur = g.db.execute('select id, movie_name, poster from movies')  #selects data from database
+    movies = [dict(id=row[0], movie_name=row[1], poster=row[2]) for row in cur.fetchall()] # adds data to dictionary
     g.db.close()
     return render_template('main.html', movies=movies, name=name)
+
+@app.route('/movies/<id>/')
+def movie(id=None):
+    g.db = connect_db()
+    cur = g.db.execute('select id, movie_name, director, year, gross, poster, category, studio, rating, synopsis from movies where id = ?', [id])
+    movies = [dict(id=row[0], movie_name=row[1], director=row[2], year=row[3], gross=row[4], poster=row[5], category=row[6], studio=row[7], rating=row[8], synopsis=row[9]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('individualmovie.html', movies=movies, id=id)
 
 @app.route('/movies/genres/<name>/')
 def genres(name=None):
@@ -20,7 +28,7 @@ def genres(name=None):
     return render_template('genres.html', movies=movies, name=name)
 	
 @app.route('/movies/upcoming/')
-def upcoming(name=None):
+def upcoming(name='Upcoming'):
     g.db = connect_db()
     cur = g.db.execute('select id, movie_name, poster from movies where year = "2016"')
     movies = [dict(id=row[0], movie_name=row[1], poster=row[2]) for row in cur.fetchall()]
@@ -28,7 +36,7 @@ def upcoming(name=None):
     return render_template('upcoming.html', movies=movies, name=name)
 	
 @app.route('/movies/top-rated/')
-def toprated(name=None):
+def toprated(name='Top Rated'):
     g.db = connect_db()
     cur = g.db.execute('select id, movie_name, poster from movies where rating = "5"')
     movies = [dict(id=row[0], movie_name=row[1], poster=row[2]) for row in cur.fetchall()]
---
+++ src/static/js/javascript.js
@@ -0,0 +1,5 @@
+$(document).ready( function() {
+    $('#summary').hover( function() {
+        $('#detail').toggle();
+    });
+});
